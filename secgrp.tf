a5fc89a48bdb632fe7246b6b089f6f3232eafbef
vupham0402 <vupham0402@gmail.com>
2023-07-21 02:44:25 -0400

    security group

--- secgrp.tf
+++ secgrp.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "vprofile-bastion-sg" {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
-    cidr_blocks = [var.MYIP]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
